d0e422c1fe315f13efa53ccdb2091d3932472a76
hagathagabriele <127766811+hagathagabriele@users.noreply.github.com>
2023-10-23 12:22:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,20 +3,25 @@ function tocaSom(idElementoAudio){
 } 
  const listaDeTeclas = document.querySelectorAll('.tecla');
  
- let contador = 0;
  //Estrutura de repetição - Enquanto
- while(contador<listaDeTeclas.length){
+for(let contador=0;contador<listaDeTeclas.length;contador++) {
     const tecla = listaDeTeclas[contador];
-       const instrumento = tecla.classList[1];
-          const idAudio = `#som_ ${instrumento}`;
-       console.log(instrumento);
-       tecla.onclick = function() {
-          tocaSom(idAudio);
-  };
-  contador = contador + 1;
-    console.log(contador);
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_ ${instrumento}`;
     console.log(idAudio);
- }
+
+      tecla.onclick = function(){
+        tocaSom(idAudio);
+      };
+      tecla.onkeydown = function(){
+         tecla.classList.add('ativa');
+      }
+      tecla.onkeyup = function(){
+         tecla.classList.remove('ativa');
+      }
+console.log(contador);
+}
+
  
  
 
